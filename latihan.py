f14ca9ea4ce7f8ae50abf85450b295e4ed226b5d
Ridwanwildan <rdwnwldn@gmail.com>
2021-10-25 20:51:28 +0700

    Menambahkan file latihan

---
+++ latihan.py
@@ -0,0 +1,21 @@
+print("Hello")
+print("Saya sedang belajar python")
+
+a = 8
+b = 6
+print("Variable a :",a)
+print("Variable b :",b)
+print ("Hasil penjumlahan a+b :",a+b)
+
+a =input("Masukkan nilai a :")
+b =input("Masukkan nilai b :")
+print("Variable a :",a)
+print("Variable b :",b)
+print("Hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+#konversi nilai variable
+
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("Hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
