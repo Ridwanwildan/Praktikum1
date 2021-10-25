91733dde7f69cc9f01ff3f5ab63f9949035a0289
Ridwanwildan <rdwnwldn@gmail.com>
2021-10-25 20:52:01 +0700

    Menambahkan file tugas

---
+++ tugas.py
@@ -0,0 +1,18 @@
+#Input data
+
+namalengkap = input("Tulis nama lengkapmu : ")
+namapanggilan = input("Tulis nama panggilanmu : ")
+NPM = input("NPM : ")
+tempatlahir = input("Tulis asal daerahmu : ")
+umur = input("Berapa usiamu ? ")
+alamat = input("Tulis alamatmu : ")
+telpon = input("Tulis nomor teleponmu : ")
+
+#Output data
+
+print("Assalamu'alaikum. \n"
+"Let me introduce my self. My name is",namalengkap,", but you can call me \n"
+,namapanggilan,".""my NPM is",NPM,"." "I was born in",tempatlahir,"and i am",umur,"years old. \n"
+"I am very glad if you want to invite my house in",alamat,". \n"
+"so, dont dorget to call me before with number",telpon,"."
+"\n \n Thank you.")
